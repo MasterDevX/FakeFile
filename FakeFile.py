138746e194f1386e5ed912ab97ef8a6b58a339a5
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-03-05 16:56:04 +0200

    Changed writing method

--- FakeFile.py
+++ FakeFile.py
@@ -1,8 +1,6 @@
 import sys
-import random
 
 Ver = 'v1.0'
-CharKey = '''(q7.бгХюR5p\жТ+$GйiПmЫ’&фKЮНузГЛэпсоLВ*Щ«,6ъцMJ1;ІАtw!l~D<УЧ0bCРкнegzЭ9IОхшr>лч4ФOщfY_Й|'%kЪдUКdЁ[N2XЦ"aрҐ/ЗAVяДP#]W:їj ^Иu?БЄyS)vавBQЕc=тH»n3єёыЯШЖhімoь8T{ZF-}ЇЬґеСМ`xиs@E'''
 
 if len(sys.argv) < 3:
 
@@ -20,13 +18,12 @@ if len(sys.argv) < 3:
     print()
     sys.exit()
 
-CharList = [i for i in CharKey]
 FileName = str(sys.argv[1])
 FileSize = int(sys.argv[2])
-File = open (FileName, 'a')
+File = open (FileName, 'wb')
 
 print('[FakeFile] Generating ' + FileName + ', ' + str(FileSize) + ' bytes...')
-
-for i in range(FileSize):
-    File.write(CharList[random.randint(0, (len(CharKey) - 1))])
+File.write(b'\0' * FileSize)
+print('[FakeFile] File created!')
 File.close()
+sys.exit()
