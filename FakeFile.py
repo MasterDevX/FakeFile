26910155b2177c4e875ee582094dcef3529dff0c
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-03-07 10:53:40 +0200

    Added size unit selection

--- FakeFile.py
+++ FakeFile.py
@@ -1,29 +1,60 @@
 import sys
 
-Ver = 'v1.0'
+Ver = 'v1.1'
 
-if len(sys.argv) < 3:
+def usage():
 
     print()
-    print('     FakeFile ' + Ver + ' by MasterDevX')
+    print('FakeFile ' + Ver + ' by MasterDevX')
     print()
-    print('     Usage:')
-    print('     FakeFile.py FileName FileSize')
+    print('Usage:')
+    print('FakeFile.py FileName FileSize SizeUnit')
     print()
-    print('     FileName - Name and extention (if needed) of the output file.')
-    print('                Examples: file, program.exe, project.veg, movie.mp4...')
+    print('FileName - Name and extention (if needed) of the output file.')
+    print('           Examples: file, program.exe, project.veg, movie.mp4...')
     print()
-    print('     FileSize - Size of output file in bytes.')
-    print('                Examples: 128, 8192, 45000...')
+    print('FileSize - Size of output file.')
+    print('           Examples: 4, 128, 4000, 8192...')
+    print()
+    print('SizeUnit - Unit, in which file size is specified:')
+    print('           B - Bytes, K - Kilobytes, M - Megabytes, G - Gigabytes.')
     print()
     sys.exit()
 
-FileName = str(sys.argv[1])
-FileSize = int(sys.argv[2])
-File = open (FileName, 'wb')
+try:
+
+    FileName = str(sys.argv[1])
+    FileSize = float(sys.argv[2])
+    SizeUnit = str(sys.argv[3])
+    File = open(FileName, 'wb')
+
+except:
+
+    usage()
+
+if SizeUnit == 'B':
+
+    FileSize = round(float(FileSize))
+
+elif SizeUnit == 'K':
+
+    FileSize = round(float(FileSize * 1024))
+
+elif SizeUnit == 'M':
+
+    FileSize = round(float(FileSize * (1024 ** 2)))
+
+elif SizeUnit == 'G':
+
+    FileSize = round(float(FileSize * (1024 ** 3)))
+
+else:
+
+    usage()
 
 print('[FakeFile] Generating ' + FileName + ', ' + str(FileSize) + ' bytes...')
 File.write(b'\0' * FileSize)
 print('[FakeFile] File created!')
+
 File.close()
 sys.exit()
